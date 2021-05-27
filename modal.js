39fd68844ad1a66f8a55649be365612497da2d1d
Caro-L-dev <liardcar@gmail.com>
2021-05-27 17:47:23 +0200

    :hammer::iphone: Gallery section responsive

--- modal.js
+++ modal.js
@@ -3,17 +3,23 @@ const facilitiesModal = document.querySelector('.bloc-modal');
 const closeModal = document.querySelector('.close-modal');
 const imgIndex = document.querySelector('.bloc-modal img');
 
-btns.forEach(btn => {
-    btn.addEventListener('click', (event) => {
-        /* Select the image directly with its associated data-index number */
-        imgIndex.src = `Styles/ressources/img${event.target.getAttribute
-        ('data-index')}-equip.jpg`;
+/* "matchMedia" method that returns true or false depending on a certain screen size (responsive) */
+/* If above 850px return true, if under 850px return false and this script is not run */
+if(window.matchMedia("(min-width: 850px").matches){
+    btns.forEach(btn => {
+        btn.addEventListener('click', (event) => {
+            /* Select the image directly with its associated data-index number */
+            imgIndex.src = `Styles/ressources/img${event.target.getAttribute
+            ('data-index')}-equip.jpg`;
+    
+            facilitiesModal.classList.add('active-modal');
+        })
+    });
+    
+    /* If I click anywhere in the category facilities, it closes the current picture */
+    facilitiesModal.addEventListener('click', () => {
+        facilitiesModal.classList.remove('active-modal');
+    });
+}
 
-        facilitiesModal.classList.add('active-modal');
-    })
-});
 
-/* If I click anywhere in the category facilities, it closes the current picture */
-facilitiesModal.addEventListener('click', () => {
-    facilitiesModal.classList.remove('active-modal');
-})
